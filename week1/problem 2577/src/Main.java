fd8e1a2424829a280ecd74b934ab8c167b2e2579
SongeunKim <ucss22002@gmail.com>
2024-09-05 12:52:25 +0900

    [ADD] 2577 숫자의 개수

--- week1/problem 2577/src/Main.java
+++ week1/problem 2577/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int result = a*b*c;
+        boolean line = false;
+        int digit = 0;
+
+        //result는 7~9자리 수
+        int arr[] = new int[10];
+        for(int i=8; i>=0; i--) {
+            arr[i] = result / (int)(Math.pow(10, i));
+            result -= arr[i] * (int)(Math.pow(10, i));
+            System.out.println("arr[" + i + "] : " + arr[i]);
+            System.out.println("result : " + result);
+            System.out.println();
+            if (arr[i] != 0 && !line) {
+                digit = i;
+                line = true;
+            }
+        }
+        System.out.println("digit : " + digit);
+
+        for (int i = 0; i < 10; i++) {
+            int count = 0;
+            for (int j = 0; j <= digit; j++) {
+                if(arr[j] == i) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+
     }
 }
