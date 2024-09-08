132a4b285a289e8849875515521fb4461266aea3
SongeunKim <ucss22002@gmail.com>
2024-09-08 22:26:33 +0900

    [SOLVED] 1475 방 번호

--- week1/problem 1475/src/Main.java
+++ week1/problem 1475/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String str = scan.nextLine();
+        int arr[] = new int[10];
+
+        for(int i = 0; i < str.length(); i++) {
+            int num = Integer.parseInt(str.substring(i, i+1));
+
+            if(num == 9) arr[6]++;
+            else arr[num]++;
+        }
+
+        if (arr[6] % 2 == 0) arr[6] = arr[6] / 2;
+        else arr[6] = arr[6]/2+1;
+
+        int max = 0;
+        for(int i = 0; i < 9; i++) {
+            max = Math.max(max, arr[i]);
+        }
+
+        System.out.println(max);
     }
+
 }
