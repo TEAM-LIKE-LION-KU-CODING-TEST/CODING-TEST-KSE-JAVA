2c343c4932e64301321163b80bcd1f0ed1b1ea8e
SongeunKim <ucss22002@gmail.com>
2024-09-09 19:52:17 +0900

    [FEAT] : 5397 키로거

--- 04_LinkedList/problem 5397/src/Main.java
+++ 04_LinkedList/problem 5397/src/Main.java
@@ -1,8 +1,53 @@
+import java.io.*;
+import java.util.LinkedList;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        System.out.println("Hello world!");
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            String str = br.readLine();
+            LinkedList<String> list = new LinkedList<>();
+            list.add("!");
+            for (int j = 0; j < str.length(); j++) {
+                String token = str.substring(j, j+1);
+                int cursor = list.indexOf("!");
+
+                if (token.equals("<")) {
+                    if (cursor != 0) {
+                        String temp = list.get(cursor-1);
+                        list.set(cursor-1, list.get(cursor));
+                        list.set(cursor, temp);
+                    }
+                }
+                else if (token.equals(">")) {
+                    if (cursor != list.size()-1) {
+                        String temp = list.get(cursor + 1);
+                        list.set(cursor + 1, list.get(cursor));
+                        list.set(cursor, temp);
+                    }
+                }
+                else if(token.equals("-")) {
+                    if (cursor != 0) {
+                        list.remove(cursor - 1);
+                    }
+                }
+                else list.add(cursor, token);
+
+            }
+            String answer = "";
+            while(list.size() > 1) {
+                answer += list.removeFirst();
+
+            }
+            bw.write(answer + "\n");
+        }
+        bw.flush();
+        bw.close();
 
     }
 }
