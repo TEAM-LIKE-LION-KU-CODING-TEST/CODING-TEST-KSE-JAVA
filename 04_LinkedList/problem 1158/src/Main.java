cc359136a58f2e9cfef505d230d07c92f802ad2c
SongeunKim <ucss22002@gmail.com>
2024-09-09 17:16:40 +0900

    [SOLVED] : 1158 요세푸스 문제

--- 04_LinkedList/problem 1158/src/Main.java
+++ 04_LinkedList/problem 1158/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList<Integer> list = new LinkedList<Integer>();
+
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int k = scan.nextInt();
