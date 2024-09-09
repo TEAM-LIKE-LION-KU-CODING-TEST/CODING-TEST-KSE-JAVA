5dfeac9175f1c8bbffd37b86ab903803ba0715d6
SongeunKim <ucss22002@gmail.com>
2024-09-09 17:14:45 +0900

    [ADD] : 1158 요세푸스 문제

--- 04_LinkedList/problem 1158/src/Main.java
+++ 04_LinkedList/problem 1158/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+
+        int num = 0;
+        System.out.print("<");
+        while(!list.isEmpty()) {
+            num = (num + k - 1) % n;
+            System.out.print(list.remove(num));
+            n--;
+
+            if(!list.isEmpty()) System.out.print(", ");
+        }
+        System.out.print(">");
     }
 }
