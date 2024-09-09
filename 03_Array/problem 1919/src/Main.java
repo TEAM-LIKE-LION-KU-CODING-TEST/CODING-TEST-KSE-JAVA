6c0217494b2fb13e047103355354dcdc7e646996
SongeunKim <ucss22002@gmail.com>
2024-09-09 22:59:34 +0900

    [FEAT] : 1919 애너그램 만들기

--- 03_Array/problem 1919/src/Main.java
+++ 03_Array/problem 1919/src/Main.java
@@ -1,6 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
 
-        System.out.println("Hello world!");
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int word[] = new int[26];
+
+        String str1 = br.readLine();
+        for (int j = 0; j < str1.length(); j++) {
+            char token = str1.charAt(j);
+            word[(int)token - 97]++;
+        }
+
+        String str2 = br.readLine();
+        for (int j = 0; j < str2.length(); j++) {
+            char token = str2.charAt(j);
+            word[(int)token - 97]--;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            sum += Math.abs(word[i]);
+        }
+
+        bw.write(sum + "");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
