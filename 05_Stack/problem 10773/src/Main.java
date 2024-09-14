55ed30e624568cad6e17801004177f3c9ae2b742
SongeunKim <ucss22002@gmail.com>
2024-09-15 00:17:45 +0900

    [SOLVED] : 10773 제로

--- 05_Stack/problem 10773/src/Main.java
+++ 05_Stack/problem 10773/src/Main.java
@@ -1,7 +1,33 @@
+import java.io.*;
+import java.util.Stack;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        System.out.println("Hello world!");
+        Stack<Integer> stack = new Stack<>();
+
+        int K = Integer.parseInt(br.readLine());
+
+        stack.add(0);
+
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) stack.pop();
+            else stack.add(num);
+        }
+
+        int sum = 0;
+        for (int i : stack)
+            sum += i;
+
+        bw.write(sum + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
