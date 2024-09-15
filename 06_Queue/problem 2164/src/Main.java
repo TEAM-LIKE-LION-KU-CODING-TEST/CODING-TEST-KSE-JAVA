ba6e23d9007843184c60018dc8895e17ce28e136
SongeunKim <ucss22002@gmail.com>
2024-09-15 22:19:47 +0900

    [SOLVED] : 2164 카드

--- 06_Queue/problem 2164/src/Main.java
+++ 06_Queue/problem 2164/src/Main.java
@@ -1,5 +1,31 @@
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+
+        while (q.size() > 1) {
+            q.remove();
+            q.add(q.poll());
+        }
+
+        bw.write(q.peek() + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
