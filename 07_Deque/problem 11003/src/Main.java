96caace976498ce2c2171007b011888d892324ac
SongeunKim <ucss22002@gmail.com>
2024-09-22 17:04:49 +0900

    [FEAT] : 11003 최솟값 찾기
    덱 맨 앞에 항상 최솟값이 오도록

--- 07_Deque/problem 11003/src/Main.java
+++ 07_Deque/problem 11003/src/Main.java
@@ -1,5 +1,39 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer str = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(str.nextToken());
+        int L = Integer.parseInt(str.nextToken());
+
+        StringTokenizer A = new StringTokenizer(br.readLine());
+        Deque<Integer> deq = new ArrayDeque<>();
+
+        String answer = "";
+        for (int i =0; i < N; i++) {
+            if(deq.size() >= L)
+                deq.removeFirst();
+
+            int temp = Integer.parseInt(A.nextToken());
+            while (!deq.isEmpty() && temp < deq.peekLast()) {
+                deq.removeLast();
+            }
+            deq.addLast(temp);
+
+//            if(deq.size() >= L)
+//                deq.removeFirst();
+
+            System.out.println(deq);
+            System.out.println();
+        }
+
     }
 }
