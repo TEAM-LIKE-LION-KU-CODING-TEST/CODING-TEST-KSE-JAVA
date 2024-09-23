0dc1a6231b38731525c767329ce824e36d5da787
SongeunKim <ucss22002@gmail.com>
2024-09-23 17:04:57 +0900

    [SOLVED] : 2504 괄호의 값
    stack을 Integer로 변경

--- 08_StackUtil/problem 2504/src/Main.java
+++ 08_StackUtil/problem 2504/src/Main.java
@@ -7,11 +7,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Stack<Character> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
 
         String str = br.readLine();
 
         int temp = 0;
+        int result = 0;
 
         for (int i = 0; i < str.length(); i++) {
 
@@ -19,58 +20,61 @@ public class Main {
 
             switch (token) {
                 case '(':
-                    stack.push(token);
-                    System.out.println(stack);
+                    stack.push(-10); // '(' => -10
                     break;
 
                 case ')' :
-                    while(!stack.isEmpty() && (stack.peek() != '(' && stack.peek() != '[')) {
-                       temp += Character.getNumericValue(stack.pop());
+                    temp = 0;
+                    while(!stack.isEmpty() && (stack.peek() != -10 && stack.peek() != -20)) {
+                       temp += stack.pop();
                     }
-                    if(!stack.isEmpty() && stack.peek() == '(') {
+                    if(!stack.isEmpty() && stack.peek() == -10) {
                         if(temp == 0) temp++;
                         stack.pop();
-                        stack.push((char) (temp * 2));
-                        temp = 0;
+                        stack.push(temp * 2);
                     }
                     else {
                         bw.write("0");
                         bw.flush();
                         System.exit(0);
                     }
-                    System.out.println(stack);
                     break;
 
                 case '[':
-                    stack.push(token);
-                    System.out.println(stack);
+                    stack.push(-20); // '[' => -20
                     break;
 
                 case ']':
-                    while(!stack.isEmpty() && (stack.peek() != '(' && stack.peek() != '[')) {
-                        temp += Character.getNumericValue(stack.pop());
+                    temp = 0;
+                    while(!stack.isEmpty() && (stack.peek() != -10 && stack.peek() != -20)) {
+                        temp += stack.pop();
                     }
-                    if(!stack.isEmpty() && stack.peek() == '[') {
+                    if(!stack.isEmpty() && stack.peek() == -20) {
                         if(temp == 0) temp++;
                         stack.pop();
-                        stack.push((char) (temp * 3));
-                        temp = 0;
+                        stack.push(temp * 3);
                     }
                     else {
                         bw.write("0");
                         bw.flush();
                         System.exit(0);
                     }
-                    System.out.println(stack);
                     break;
             }
         }
 
+        // 스택에 남아 있는 값을 모두 더해 최종 결과를 계산
         while (!stack.isEmpty()) {
-            temp += Character.getNumericValue(stack.pop());
+            int value = stack.pop();
+            if (value < 0) {  // 스택에 여는 괄호가 남아있으면 잘못된 입력
+                bw.write("0");
+                bw.flush();
+                System.exit(0);
+            }
+            result += value;
         }
 
-        bw.write(temp + "");
+        bw.write(result + "");
 
         bw.flush();
         bw.close();
