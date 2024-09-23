194bc4088f7ce47f6dbdc9381c5e61efef0c76c2
SongeunKim <ucss22002@gmail.com>
2024-09-23 15:26:54 +0900

    [FEAT] : 2504 괄호의 값
    뭔가가 잘못됨

--- 08_StackUtil/problem 2504/src/Main.java
+++ 08_StackUtil/problem 2504/src/Main.java
@@ -1,16 +1,65 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        Stack<Character> stack = new Stack<>();
 
+        String str = br.readLine();
 
-        System.out.println("Hello world!");
+        int answer = 1;
+
+        Character last = null;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            Character token = str.charAt(i);
+
+            switch (token) {
+                case '(':
+                    stack.push(token);
+                    break;
+
+                case ')' :
+                    if(stack.peek() == '(')
+                        stack.pop();
+                    else {
+                        bw.write("0");
+                        bw.flush();
+                        System.exit(0);
+                    }
+                    answer *= 2;
+                    break;
+
+                case '[':
+                    stack.push(token);
+                    break;
+
+                case ']':
+                    if(stack.peek() == '[')
+                        stack.pop();
+                    else {
+                        bw.write("0");
+                        bw.flush();
+                        System.exit(0);
+                    }
+                    answer *= 3;
+                    break;
+            }
+
+            last = token;
+        }
+
+        if (!stack.isEmpty()) answer = 0;
+
+        bw.write(answer + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
