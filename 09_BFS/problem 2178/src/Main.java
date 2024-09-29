18dab1f5b299777e36d0492593a895610a7bdc73
SongeunKim <ucss22002@gmail.com>
2024-09-29 17:43:11 +0900

    [ADD] : 2178 미로 탐색

---
+++ 09_BFS/problem 2178/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // N, M 읽어오기
+        String[] line = br.readLine().split(" ");
+        int N = Integer.parseInt(line[0]);
+        int M = Integer.parseInt(line[1]);
+
+    }
+}
