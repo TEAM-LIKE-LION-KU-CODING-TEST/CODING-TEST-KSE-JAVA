40563b7e52f90757222bc6d3021eb6024b80d432
SongeunKim <ucss22002@gmail.com>
2024-09-30 18:03:11 +0900

    [FEAT] : 3197 백조의 호수
    얼음 녹는 거 구현 완

--- 09_BFS/problem 3197/src/Main.java
+++ 09_BFS/problem 3197/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,7 +8,7 @@ public class Main {
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
     static String[][] lake;
-    static Queue<Point> q;
+    static Queue<Point> ice;
     static Queue<Point> swan;
     public static void main(String[] args) throws IOException {
 
@@ -20,14 +21,16 @@ public class Main {
         int C = Integer.parseInt(line[1]);
 
         lake = new String [R][C];
+        ice = new LinkedList<>();
+        swan = new LinkedList<>();
 
         for (int i = 0; i < R; i++) {
             String str = br.readLine();
             for(int j = 0; j < C; j++) {
                 lake[i][j] = String.valueOf(str.charAt(j));
 
-                if(lake[i][j].equals("x")) {
-                    q.add(new Point(i, j));
+                if(lake[i][j].equals("X")) {
+                    ice.add(new Point(i, j));
                 }
                 else if(lake[i][j].equals("L")) {
                     swan.add(new Point(i, j));
@@ -40,16 +43,48 @@ public class Main {
 
     public static void BFS() {
 
-        Queue<Point> q = new LinkedList<>();
+        Queue<Point> melt = new LinkedList<>();
+        while(!ice.isEmpty()) {
 
-        Point temp = q.poll();
+            int iceSize = ice.size();
+            for (int l = 0; l < iceSize; l++) {
 
-        // 상하좌우 이동
-        for (int i = 0; i < 4; i++) {
-            int nx = temp.x + dx[i];
-            int ny = temp.y + dy[i];
+                Point temp = ice.poll();
+                Boolean isMelt = false;
 
+                // 상하좌우 이동
+                for (int i = 0; i < 4; i++) {
 
+                    int nx = temp.x + dx[i];
+                    int ny = temp.y + dy[i];
+
+                    if (nx >= 0 && nx < lake.length && ny >= 0
+                        && ny < lake[0].length && lake[nx][ny].equals(".")) {
+                        melt.add(temp);
+                        isMelt = true;
+                        break;
+                    }
+                }
+
+                if (!isMelt) {
+                    ice.add(temp);
+                }
+            }
+
+            while (!melt.isEmpty()) {
+                Point temp = melt.poll();
+                lake[temp.x][temp.y] = ".";
+            }
+
+            String str = "";
+            for (int x = 0; x < lake.length; x++) {
+                for (int y = 0; y < lake[0].length; y++) {
+                    str += lake[x][y];
+                }
+                str += "\n";
+            }
+            System.out.println(str);
+            System.out.println();
         }
     }
 }
